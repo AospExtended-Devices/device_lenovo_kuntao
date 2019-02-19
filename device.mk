f5721fc3aec301b271b622e825129f999eaa7411
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2019-02-19 17:32:06 +0530

    Revert "kuntao: Enforce RRO on framework-res"
    This reverts commit f541f1c6479fc052ce5fce8dba6639b98180c269.
    
    Change-Id: Ifef341a263e79a88a45ff7537bf3e157bc2ad190

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
