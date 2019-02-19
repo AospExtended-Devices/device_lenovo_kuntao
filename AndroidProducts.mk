0fc4370cc6e2be0ef461e8885edf089c6c405f47
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2019-02-19 18:36:42 +0530

    kuntao: AEX
    Change-Id: Ief693982a538dd6185fe68fe08105c127ef7d3b9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_kuntao.mk
+    $(LOCAL_DIR)/aosp_kuntao.mk
--- aosp_kuntao.mk
+++ aosp_kuntao.mk
@@ -15,8 +15,7 @@
 #
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -26,13 +25,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/kuntao/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_kuntao
+PRODUCT_NAME := aosp_kuntao
 PRODUCT_DEVICE := kuntao
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo P2a42
@@ -45,3 +44,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="P2a42"
 
 BUILD_FINGERPRINT := Lenovo/kuntao_row/P2a42:7.0/NRD90N/P2a42_S251_171107_ROW:user/release-keys
+
+TARGET_USE_JELLY := true
+
+EXTENDED_BUILD_TYPE := OFFICIAL
