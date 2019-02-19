c1b544e3acea32be1e7677a88893669c5da56e3e
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2019-02-19 19:19:13 +0530

    kuntao: Build LenovoParts
    Change-Id: I6d6dc101cf60a7d4b18cf32ec22621e0d32b44a4

--- device.mk
+++ device.mk
@@ -193,6 +193,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# LenovoParts
+PRODUCT_PACKAGES += \
+    LenovoParts
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.kuntao
